1ae204e52d6d2a1907cf57287e33beb8d268ca68
Rambod Ghashghai <gh.rambod@gmail.com>
2023-11-29 15:54:23 +0330

    Enhance Game Initialization and Input Handling
    - Refactored the main game window setup by introducing `GameSettings` struct.
    - Integrated the assets folder and loaded the DemoMap texture for the game map.
    - Initialized audio device and added background music streaming with "the-white-lion.mp3".
    - Implemented basic WASD keyboard input for map movement.
    - Utilized raymath for vector operations to move the map based on player input.
    - Adjusted game window dimensions and scaling for map rendering.
    - Cleaned up resources by unloading textures and closing the audio device before exiting.

--- main.cpp
+++ main.cpp
@@ -1,25 +1,85 @@
 #include <iostream>
 #include <raylib.h>
 #include <raygui.h>
+#include <raymath.h>
+
+struct GameSettings {
+    int width;
+    int height;
+};
 
 int main() {
+    GameSettings gameSettings = {
+        .width = 384,
+        .height = 384
+    };
+    InitWindow(gameSettings.width, gameSettings.height, "CppTopDownTemplate");
 
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    // Texture Paths
+    const char* mapTexture = "assets/DemoMap.png";
+
+
+    //Audio Device
+    InitAudioDevice();
+
+    //Load Music and Sounds
+    Music music = LoadMusicStream("assets/Sounds/the-white-lion.mp3");
+    //Play Music
+    PlayMusicStream(music);
+
+    // Tilemap Settings
+    Texture2D mapTexture2D = LoadTexture(mapTexture);
+    Vector2 mapPos{0.0f,0.0f};
+    float speed = 4.0f;
+
+
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+        UpdateMusicStream(music);
 
+        // Begin Drawing
         BeginDrawing();
 
-        ClearBackground(BLACK);
+        // Clear Screen
+        ClearBackground(WHITE);
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A))
+            direction.x -= 1.0;
+        if(IsKeyDown(KEY_D))
+            direction.x += 1.0;
+        if(IsKeyDown(KEY_W))
+            direction.y -= 1.0;
+        if(IsKeyDown(KEY_S))
+            direction.y += 1.0;
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, WHITE);
+        if(Vector2Length(direction) > 0.0f){
+
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
 
 
+
+        // Draw Map
+        DrawTextureEx(
+                mapTexture2D,
+                mapPos,
+                0.0f,
+                4.0f,
+                WHITE
+        );
+
+
+
+
+        // End Drawing
         EndDrawing();
 
     }
+
+    // Unload Texture
+    UnloadTexture(mapTexture2D);
 
     CloseWindow();
 
